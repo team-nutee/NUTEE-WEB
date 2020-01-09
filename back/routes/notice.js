34257f4cde077dc89cfc127e82e26a47adba47dd
dladncks1217 <dlaxodud1217@gmail.com>
2020-01-09 17:25:56 +0900

    [feature/chatting] #13 Refactor Notice crawl Router

--- back/routes/notice.js
+++ back/routes/notice.js
@@ -8,33 +8,34 @@ const index4 = require('../crawl/index4');
 const index5 = require('../crawl/index5');
 const index6 = require('../crawl/index6');
 
-router.get('/',(req,res,next)=>{
-    let constresult = {};
-    index1.then((results)=>{
-        console.log(results);
-         constresult.a = results;
-        index2.then((results)=>{
-            constresult.b = results;
-            index3.then((results)=>{
-                constresult.c = results;
-                index4.then((results)=>{
-                    constresult.d = results;
-                    index5.then((results)=>{
-                        constresult.e = results;
-                        index6.then((results)=>{
-                            constresult.f = results;
-                        });
-                    });
-                });
-            });
-        });
-    });
-    setTimeout(()=>{
-        console.log(constresult.a);
-        console.log(constresult.b);
-        console.dir("constresult는"+constresult.toLocaleString());
-        res.json(constresult);
-    },500);
+router.get('/',async(req,res,next)=>{
+    try{
+        let crawlresult = {
+            'crawl1': await index1.then((result)=>{
+                return result;
+            }),
+            'crawl2': await index2.then((result)=>{
+                return result;
+            }),
+            'crawl3':await index3.then((result)=>{
+                return result;
+            }),
+            'crawl4':await index4.then((result)=>{
+                return result;
+            }),
+            'crawl5': await index5.then((result)=>{
+                return result;
+            }),
+            'crawl6':await index6.then((result)=>{
+                return result;
+            }),
+        };
+        res.json(crawlresult);
+    }catch(err){
+        console.error(err);
+        next(err);
+    }
 });
+
 
 module.exports = router;
