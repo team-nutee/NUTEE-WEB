650e3109211790ea69eefccef770a4466c1b0b71
dladncks1217 <dlaxodud1217@gmail.com>
2020-02-02 15:40:47 +0900

    [feature/refactoring] #13 Refactor Crawling Code

--- back/routes/notice.js
+++ back/routes/notice.js
@@ -4,22 +4,17 @@ const router = express.Router();
 const index1 = require('../crawl/index');
 const href = require('../crawl/href');
 
-router.get('/content',async(req,res,next)=>{
+router.get('/',async(req,res,next)=>{
     try{
-        let content = await index1;
-        res.json(content);
+        let crawlresult = {
+            content: await index1,
+            hrefs: await href
+        };
+        res.json(crawlresult);
     }catch(err){
         console.error(err);
         next(err);
     }
 });
-router.get('/href', async(req,res,next)=>{
-    try{
-        let hrefs = await href;
-        res.json(hrefs);
-    }catch(err){
-        console.error(err);
-        next(err);
-    }
-});
+
 module.exports = router;
