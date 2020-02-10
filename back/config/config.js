961879de8b03b82e4aa149b68ab53cb0d581c2af
MoonHKLee <mf311110@gmail.com>
2020-02-11 03:41:46 +0900

    diploy

--- back/config/config.js
+++ back/config/config.js
@@ -4,30 +4,30 @@ dotenv.config();
 
 module.exports={
   development: {
-    username: 'root',
+    username: 'nutee',
     password: process.env.DB_PASSWORD,
-    database: 'nutee',
-    host: '127.0.0.1',
+    database: 'nutee_test',
+    host: 'nuteedb.cmwlqxe5se7d.ap-northeast-2.rds.amazonaws.com',
     dialect: 'mysql',
-    port:3307,
+    port:3306,
     operatorsAliases: false
   },
   test: {
-    username: 'root',
+    username: 'nutee',
     password: process.env.DB_PASSWORD,
-    database: 'nutee',
-    host: '127.0.0.1',
+    database: 'nutee_test',
+    host: 'nuteedb.cmwlqxe5se7d.ap-northeast-2.rds.amazonaws.com',
     dialect: 'mysql',
-    port:3307,
+    port:3306,
     operatorsAliases: false
   },
   production: {
-    username: 'root',
+    username: 'nutee',
     password: process.env.DB_PASSWORD,
-    database: 'nutee',
-    host: '127.0.0.1',
+    database: 'nutee_test',
+    host: 'nuteedb.cmwlqxe5se7d.ap-northeast-2.rds.amazonaws.com',
     dialect: 'mysql',
-    port:3307,
+    port:3306,
     operatorsAliases: false
   }
 };
