33f7b1f9540d5b1c829755eff863f43360e4d87a
MoonHKLee <mf311110@gmail.com>
2020-04-23 14:21:52 +0900

    [front] 배포/개발/테스트 환경 분리

--- front/static.js
+++ front/static.js
@@ -1,4 +1,7 @@
-export const TARGET_URL ='http://localhost:9425';// http://13.124.232.115:9425 http://localhost:9425
+// http://localhost:9425      ->dev
+// http://13.124.232.115:9425 ->test
+// http://13.124.232.115:9596 ->prod
+export const TARGET_URL ='http://13.124.232.115:9596';
 export const MEAL_URL = {
     "url":"https://skhu.ac.kr/uni_zelkova/uni_zelkova_4_3_view.aspx?idx=374\u0026curpage=1"
 };
