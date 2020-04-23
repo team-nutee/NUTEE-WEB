cb684b53efa0560456a3600987cb5fe451357581
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-04-24 00:27:32 +0900

    [feature/fixed_notice] #94 고정 공지사항 추가

--- back/crawl/href.js
+++ back/crawl/href.js
@@ -4,7 +4,8 @@ const {JSDOM} = jsdom;
 
 let crawlresult = [];
 
-const crawl = (className1,className2,className3,className4,className5,className6,url) => new Promise((resolve)=>{
+const crawl = (className1,className2,className3,className4,className5,className6,className7, className8, className9,
+               className10, className11, className12, className13, className14,className15, url) => new Promise((resolve)=>{
     client.fetch(url, {},(err, $, res, body)=> {
         let notice = [];
         var body = $.html();
@@ -17,35 +18,98 @@ const crawl = (className1,className2,className3,className4,className5,className6
         let resultsplit = beforeresult.split('&amp;');
         notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
 
-        check = dom.window.document.getElementById(className2).innerHTML; // 공지제외 1번
+        check = dom.window.document.getElementById(className2).innerHTML;
         str1 = check.indexOf('?');
         str2 = check.indexOf('\"',str1+1);
         beforeresult = check.substring(str1,str2);
         resultsplit = beforeresult.split('&amp;');
         notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
 
-        check = dom.window.document.getElementById(className3).innerHTML;  // 공지제외 2번
+        check = dom.window.document.getElementById(className3).innerHTML;
         str1 = check.indexOf('?');
         str2 = check.indexOf('\"',str1+1);
         beforeresult = check.substring(str1,str2);
         resultsplit = beforeresult.split('&amp;');
         notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
 
-        check = dom.window.document.getElementById(className4).innerHTML;  // 공지제외 3번
+        check = dom.window.document.getElementById(className4).innerHTML;
         str1 = check.indexOf('?');
         str2 = check.indexOf('\"',str1+1);
         beforeresult = check.substring(str1,str2);
         resultsplit = beforeresult.split('&amp;');
         notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
 
-        check = dom.window.document.getElementById(className5).innerHTML;  // 공지제외 4번
+        check = dom.window.document.getElementById(className5).innerHTML;
         str1 = check.indexOf('?');
         str2 = check.indexOf('\"',str1+1);
         beforeresult = check.substring(str1,str2);
         resultsplit = beforeresult.split('&amp;');
         notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
 
-        check = dom.window.document.getElementById(className6).innerHTML;  // 공지제외 5번
+        check = dom.window.document.getElementById(className6).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className7).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className8).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className9).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className10).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className11).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className12).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className13).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className14).innerHTML;
+        str1 = check.indexOf('?');
+        str2 = check.indexOf('\"',str1+1);
+        beforeresult = check.substring(str1,str2);
+        resultsplit = beforeresult.split('&amp;');
+        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+
+        check = dom.window.document.getElementById(className15).innerHTML;
         str1 = check.indexOf('?');
         str2 = check.indexOf('\"',str1+1);
         beforeresult = check.substring(str1,str2);
@@ -57,68 +121,122 @@ const crawl = (className1,className2,className3,className4,className5,className6
 });
 
 //학사
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle", // [공지] 위부터 11개
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl01_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl02_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl03_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl04_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl05_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl06_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl07_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl08_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl09_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl10_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
     'http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10004&searchBun=51')
     .then(result =>{
         crawlresult[0] = result;
     });
 
 //수업
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
+crawl("ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle", // [공지] 없음
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl08_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl09_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl10_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl11_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl12_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl13_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl14_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10005&searchBun=53")
     .then(result =>{
         crawlresult[1] = result;
     });
 //학점
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle", // [공지] 위 1개
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl08_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl09_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl10_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl11_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl12_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl13_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10038&searchBun=89")
     .then(result =>{
         crawlresult[2] = result;
     });
 //장학
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle", // [공지] 위부터 9개
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl01_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl02_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl03_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl04_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl05_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl06_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl07_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl08_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10006&searchBun=75")
     .then(result =>{
         crawlresult[3] = result;
     });
 //일반
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle", // [공지] 위부터 7개
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl01_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl02_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl03_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl04_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl05_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl06_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10007")
     .then(result =>{
         crawlresult[4] = result;
     });
 //행사
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle", // [공지] 위 1개
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl08_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl09_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl10_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl11_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl12_lblTitle",
+    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl13_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10008")
     .then(result =>{
         crawlresult[5] = result;
--- back/crawl/index.js
+++ back/crawl/index.js
@@ -1,7 +1,8 @@
 const client = require('cheerio-httpcli');
 let crawlresult = [];
 
-const crawl = (className1,className2,className3,className4,className5,className6,url) => new Promise((resolve)=>{ // 학사
+const crawl = (className1,className2,className3,className4,className5,className6,className7, className8, className9,
+               className10, className11, className12, className13, className14,className15, url) => new Promise((resolve)=>{
     client.fetch(url, {},(err, $, res, body)=>{
         let bachelor_notice = [];
 
@@ -9,30 +10,66 @@ const crawl = (className1,className2,className3,className4,className5,className6
         let results = list.find('a').text();
         bachelor_notice.push(results);
 
-        list = $(className2); // 공지제외 1번
+        list = $(className2);
         bachelor_notice.push(list.find('a').text());
 
-        list = $(className3); // 공지제외 2번
+        list = $(className3);
         bachelor_notice.push(list.find('a').text());
 
-        list = $(className4); // 공지제외 3번
+        list = $(className4);
         bachelor_notice.push(list.find('a').text());
 
-        list = $(className5); // 공지제외 4번
+        list = $(className5);
         bachelor_notice.push(list.find('a').text());
 
-        list = $(className6); // 공지제외 5번
+        list = $(className6);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className7);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className8);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className9);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className10);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className11);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className12);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className13);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className14);
+        bachelor_notice.push(list.find('a').text());
+
+        list = $(className15);
         bachelor_notice.push(list.find('a').text());
         resolve(bachelor_notice);
     });
 });
 
 crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl01_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl02_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl03_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl04_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl05_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl06_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl07_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl08_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl09_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl10_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
     'http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10004&searchBun=51')
     .then(result =>{
         crawlresult[0] = result;
@@ -44,6 +81,15 @@ crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl08_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl09_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl10_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl11_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl12_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl13_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl14_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10005&searchBun=53")
     .then(result =>{
         crawlresult[1] = result;
@@ -55,28 +101,55 @@ crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl08_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl09_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl10_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl11_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl12_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl13_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10038&searchBun=89")
     .then(result =>{
         crawlresult[2] = result;
     });
 
 crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl01_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl02_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl03_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl04_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl05_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl06_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl07_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl08_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10006&searchBun=75")
     .then(result =>{
         crawlresult[3] = result;
     });
 
 crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl01_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl02_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl03_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl04_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl05_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl06_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10007")
     .then(result =>{
         crawlresult[4] = result;
@@ -88,6 +161,15 @@ crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
     ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl06_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl07_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl08_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl09_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl10_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl11_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl12_lblTitle",
+    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl13_lblTitle",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10008")
     .then(result =>{
         crawlresult[5] = result;
