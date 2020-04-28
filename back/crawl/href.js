5cc5a6357ba0556301bbeb447eebd675186fa67f
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-04-28 22:57:03 +0900

    [feature/Modify_notice] #94 공지사항 크롤링 수정

--- back/crawl/href.js
+++ back/crawl/href.js
@@ -1,124 +1,67 @@
 const client = require('cheerio-httpcli');
-const jsdom = require('jsdom');
-const {JSDOM} = jsdom;
-
 let crawlresult = [];
 
-const crawl = (className1,className2,className3,className4,className5,className6,url) => new Promise((resolve)=>{
+const crawl = (className1,category, url) => new Promise((resolve)=>{
     client.fetch(url, {},(err, $, res, body)=> {
         let notice = [];
-        var body = $.html();
-        let htmlbody = body;
-        let dom = new JSDOM(htmlbody);
-        let check = dom.window.document.getElementById(className1).innerHTML; // [공지]부분
-        let str1 = check.indexOf('?');
-        let str2 = check.indexOf('\"',str1+1);
-        let beforeresult = check.substring(str1,str2);
-        let resultsplit = beforeresult.split('&amp;');
-        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
+        let tag = $(className1);
 
-        check = dom.window.document.getElementById(className2).innerHTML; // 공지제외 1번
-        str1 = check.indexOf('?');
-        str2 = check.indexOf('\"',str1+1);
-        beforeresult = check.substring(str1,str2);
-        resultsplit = beforeresult.split('&amp;');
-        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
+        let list = [];
 
-        check = dom.window.document.getElementById(className3).innerHTML;  // 공지제외 2번
-        str1 = check.indexOf('?');
-        str2 = check.indexOf('\"',str1+1);
-        beforeresult = check.substring(str1,str2);
-        resultsplit = beforeresult.split('&amp;');
-        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
+        notice.push(category);
+        tag.each(function(i, elem) {
 
-        check = dom.window.document.getElementById(className4).innerHTML;  // 공지제외 3번
-        str1 = check.indexOf('?');
-        str2 = check.indexOf('\"',str1+1);
-        beforeresult = check.substring(str1,str2);
-        resultsplit = beforeresult.split('&amp;');
-        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}&${resultsplit[3]}`);
-
-        check = dom.window.document.getElementById(className5).innerHTML;  // 공지제외 4번
-        str1 = check.indexOf('?');
-        str2 = check.indexOf('\"',str1+1);
-        beforeresult = check.substring(str1,str2);
-        resultsplit = beforeresult.split('&amp;');
-        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
-
-        check = dom.window.document.getElementById(className6).innerHTML;  // 공지제외 5번
-        str1 = check.indexOf('?');
-        str2 = check.indexOf('\"',str1+1);
-        beforeresult = check.substring(str1,str2);
-        resultsplit = beforeresult.split('&amp;');
-        notice.push(`http://www.skhu.ac.kr/board/boardread.aspx${resultsplit[0]}&${resultsplit[1]}&${resultsplit[2]}`);
+            list[i] = $(this).find('a').attr('href');
+            notice.push(`http://www.skhu.ac.kr/board/${list[i]}`);
+        });
 
         resolve(notice);
     })
 });
 
 //학사
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15 span",
+    '학사공지',
     'http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10004&searchBun=51')
     .then(result =>{
         crawlresult[0] = result;
     });
 
 //수업
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+crawl(".left15 span",
+    '수업공지',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10005&searchBun=53")
     .then(result =>{
         crawlresult[1] = result;
     });
+
 //학점
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15 span",
+    '학점교류',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10038&searchBun=89")
     .then(result =>{
         crawlresult[2] = result;
     });
+
 //장학
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15 span",
+    '장학공지',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10006&searchBun=75")
     .then(result =>{
         crawlresult[3] = result;
     });
+
 //일반
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15 span",
+    '일반공지',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10007")
     .then(result =>{
         crawlresult[4] = result;
     });
+
 //행사
-crawl("ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    "ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15 span",
+    '행사공지',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10008")
     .then(result =>{
         crawlresult[5] = result;
--- back/crawl/index.js
+++ back/crawl/index.js
@@ -1,93 +1,55 @@
 const client = require('cheerio-httpcli');
 let crawlresult = [];
 
-const crawl = (className1,className2,className3,className4,className5,className6,url) => new Promise((resolve)=>{ // 학사
+const crawl = (className1, category, url) => new Promise((resolve)=>{
     client.fetch(url, {},(err, $, res, body)=>{
         let bachelor_notice = [];
 
-        let list = $(className1); // [공지]부분
+        let list = $(className1);
         let results = list.find('a').text();
-        bachelor_notice.push(results);
+        bachelor_notice.push(category+results);
 
-        list = $(className2); // 공지제외 1번
-        bachelor_notice.push(list.find('a').text());
-
-        list = $(className3); // 공지제외 2번
-        bachelor_notice.push(list.find('a').text());
-
-        list = $(className4); // 공지제외 3번
-        bachelor_notice.push(list.find('a').text());
-
-        list = $(className5); // 공지제외 4번
-        bachelor_notice.push(list.find('a').text());
-
-        list = $(className6); // 공지제외 5번
-        bachelor_notice.push(list.find('a').text());
         resolve(bachelor_notice);
     });
 });
 
-crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15",
+    '학사공지: ',
     'http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10004&searchBun=51')
     .then(result =>{
         crawlresult[0] = result;
     });
 
-crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl05_lblTitle",
+crawl(".left15",
+    "수업공지: ",
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10005&searchBun=53")
     .then(result =>{
         crawlresult[1] = result;
     });
 
-crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15",
+    '학점교류: ',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10038&searchBun=89")
     .then(result =>{
         crawlresult[2] = result;
     });
 
-crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15",
+    '장학공지: ',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10006&searchBun=75")
     .then(result =>{
         crawlresult[3] = result;
     });
 
-crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15",
+    '일반공지: ',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10007")
     .then(result =>{
         crawlresult[4] = result;
     });
 
-crawl(".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList2_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl00_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl01_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl02_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl03_lblTitle",
-    ".left15 #ctl00_ContentPlaceHolder1_ctl00_rptList_ctl04_lblTitle",
+crawl(".left15",
+    '행사공지: ',
     "http://www.skhu.ac.kr/board/boardlist.aspx?bsid=10008")
     .then(result =>{
         crawlresult[5] = result;
