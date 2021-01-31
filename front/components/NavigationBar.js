827e5e40301169e675ed8fed00a604f7b46b194f
gwonkim <wo41896804@gmail.com>
2021-01-31 23:02:02 +0900

    NavigationBar search 수정
    아이콘 클릭 후 검색창 보이도록 수정

--- front/components/NavigationBar.js
+++ front/components/NavigationBar.js
@@ -1,13 +1,16 @@
-import React, { useCallback, useMemo } from "react";
-import { Button, Col, Input, Row } from "antd";
+import React, { useCallback, useMemo, useState } from "react";
+import { Button, Input, Row } from "antd";
+import { SearchOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import Link from "next/link";
 import Router from "next/router";
 import { useDispatch } from "react-redux";
 import { LOG_OUT_REQUEST } from "../reducers/user";
 
-const { Search } = Input;
-
 const NavigationBar = ({ me }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const onShowSearch = () => setShowSearch(true);
+  const onCloseSearch = () => setShowSearch(false);
+
   const onSearch = text => {
     Router.push(
       { pathname: "/search", query: { text: text } },
@@ -23,22 +26,29 @@ const NavigationBar = ({ me }) => {
   }, []);
 
   const wrapper = useMemo(() => ({ background: '#f0faf5', height: '65px', paddingTop: '15px', position: 'fixed', top: '0', zIndex: '5', width: '100%', }), []);
+  /* 로고 */
   const logoWrapper = useMemo(() => ({ float: "left", marginLeft: '7vw' }), []);
   const logoImgWrapper = useMemo(() => ({ height: '40px', width: '40px', marginLeft: '10px' }), []);
+
+  /* NUTEE */
   const nuteeWrapper = useMemo(() => ({ float: "left", margin: '9.5px 10px 0px 0px' }), []);
   const nuteeAWrapper = useMemo(() => ({ fontFamily: 'Do Hyeon', color: '#13c276', fontSize: '40px' }), []);
-  const searchWrapper = useMemo(() => ({ float: "left", marginLeft: '20px' }), []);
-  const inputSearchWrapper = useMemo(() => ({ verticalAlign: 'middle', width : '250px' }), []);
+
+  /* 검색 */
+  const searchWrapper = useMemo(() => ({ float: "left", margin: '0 5px 0 20px', width: '200px' }), []);
+  const showSearchWrapper = useMemo(() => ({ fontSize: '30px', color: '#005000' }), []);
+  const closeIconWrapper = useMemo(() => ({ padding: '10px' }), []);
+
+  /* 로드아웃 */
   const logoutWrapper = useMemo(() => ({ float: 'right', marginRight: '7vw', }), []);
   const logoutButtonWrapper = useMemo(() => ({ background: '#13c276', marginTop: '5px', marginRight: '10px', borderColor: '#fff', color: 'white' }), []);
 
   return (
     <Row style={wrapper}>
-      <Col span={18}>
         <div style={logoWrapper}>
           <Link href="/">
             <a>
-              <img style={logoImgWrapper} src={'/nutee_circle.png'} alt="nutee" /> 
+              <img style={logoImgWrapper} src={'/nutee_circle.png'} alt="nutee" />
             </a>
           </Link>
         </div>
@@ -49,22 +59,18 @@ const NavigationBar = ({ me }) => {
             </a>
           </Link>
         </div>
-        {me ? (
+        {me ? 
           <>
-            <div style={searchWrapper}>
-              <Search
-                placeholder="검색어를 입력하세요."
-                allowClear
-                onSearch={onSearch}
-                style={inputSearchWrapper}
-              />
-            </div>
+            {showSearch ?
+              <>
+                <Input
+                  placeholder="검색어를 입력하세요."
+                  allowClear
+                  style={searchWrapper}
+                /><SearchOutlined style={showSearchWrapper} onClick={onSearch} /><CloseCircleOutlined onClick={onCloseSearch} style={closeIconWrapper} />
+              </> : <SearchOutlined style={showSearchWrapper} onClick={onShowSearch} />}
           </>
-        ) : (
-          <></>
-        )}
-      </Col>
-      <Col span={6}>
+        : <></>}
         {me ? (
           <div style={logoutWrapper}>
             <Button
@@ -76,9 +82,8 @@ const NavigationBar = ({ me }) => {
             </Button>
           </div>
         ) : (
-          <></>
-        )}
-      </Col>
+            <></>
+          )}
     </Row>
   );
 };
