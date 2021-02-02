136b3535d6ebde5bae06152bf94d5a7ab99bc600
gwonkim <wo41896804@gmail.com>
2021-02-03 00:05:13 +0900

    NavigationBar 수정
    search : window의 크기가 특정 사이즈가 되었을 때 자동으로 검색창을 보여주고 숨기는 형태로 수정
    기타 세부 디자인 수정(색상이나 사이즈)

--- front/components/NavigationBar.js
+++ front/components/NavigationBar.js
@@ -1,89 +1,112 @@
-import React, { useCallback, useMemo, useState } from "react";
-import { Button, Input, Row } from "antd";
-import { SearchOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Button, Input, Row, Popover } from "antd";
+import { SearchOutlined } from '@ant-design/icons';
 import Link from "next/link";
 import Router from "next/router";
 import { useDispatch } from "react-redux";
 import { LOG_OUT_REQUEST } from "../reducers/user";
+import useInput from '../hooks/useInput';
 
 const NavigationBar = ({ me }) => {
+  const [searchInput, onChangeSearchInput] = useInput('');
   const [showSearch, setShowSearch] = useState(false);
-  const onShowSearch = () => setShowSearch(true);
-  const onCloseSearch = () => setShowSearch(false);
-
-  const onSearch = text => {
-    Router.push(
-      { pathname: "/search", query: { text: text } },
-      `/search/${text}`
-    );
-  };
-
   const dispatch = useDispatch();
+
+  const onSearch = useCallback(() => {
+    if (searchInput) {
+      Router.push(`/search/${searchInput}`);
+    }
+  }, [searchInput]);
+
   const onLogout = useCallback(() => {
     dispatch({
       type: LOG_OUT_REQUEST,
     });
   }, []);
 
+  useEffect(
+    function onSearchWidth() {
+      if ((window.innerWidth || document.body.clientWidth) > 700) {
+        setShowSearch(true);
+      } else {
+        setShowSearch(false);
+      }
+      window.addEventListener("resize", onSearchWidth);
+      return () => {
+        window.removeEventListener("resize", onSearchWidth);
+      };
+    });
+
   const wrapper = useMemo(() => ({ background: '#f0faf5', height: '65px', paddingTop: '15px', position: 'fixed', top: '0', zIndex: '5', width: '100%', }), []);
   /* 로고 */
   const logoWrapper = useMemo(() => ({ float: "left", marginLeft: '7vw' }), []);
-  const logoImgWrapper = useMemo(() => ({ height: '40px', width: '40px', marginLeft: '10px' }), []);
+  const logoImgWrapper = useMemo(() => ({ height: '40px', width: '40px', margin: '-1px 3px 0 5px' }), []);
 
   /* NUTEE */
-  const nuteeWrapper = useMemo(() => ({ float: "left", margin: '9.5px 10px 0px 0px' }), []);
+  const nuteeWrapper = useMemo(() => ({ float: "left", margin: '10px 10px 0px 0px' }), []);
   const nuteeAWrapper = useMemo(() => ({ fontFamily: 'Do Hyeon', color: '#13c276', fontSize: '40px' }), []);
 
   /* 검색 */
   const searchWrapper = useMemo(() => ({ float: "left", margin: '0 5px 0 20px', width: '200px' }), []);
-  const showSearchWrapper = useMemo(() => ({ fontSize: '30px', color: '#005000' }), []);
-  const closeIconWrapper = useMemo(() => ({ padding: '10px' }), []);
+  const showSearchWrapper = useMemo(() => ({ fontSize: '30px', color: '#13c276', marginTop: '2px' }), []);
 
   /* 로그아웃 */
   const logoutWrapper = useMemo(() => ({ float: 'right', marginRight: '7vw', }), []);
-  const logoutButtonWrapper = useMemo(() => ({ background: '#13c276', marginTop: '5px', marginRight: '10px', borderColor: '#fff', color: 'white' }), []);
+  const logoutButtonWrapper = useMemo(() => ({ background: '#13c276', marginRight: '10px', borderColor: '#fff', color: 'white' }), []);
+
+  const content = (
+    <>
+      <Input
+        placeholder="검색어를 입력하세요."
+        allowClear
+        style={searchWrapper}
+      /><SearchOutlined style={showSearchWrapper} onClick={onSearch} />
+    </>
+  );
 
   return (
     <Row style={wrapper}>
-        <div style={logoWrapper}>
-          <Link href="/">
-            <a>
-              <img style={logoImgWrapper} src={'/nutee_circle.png'} alt="nutee" />
-            </a>
-          </Link>
-        </div>
-        <div style={nuteeWrapper}>
-          <Link href="/">
-            <a>
-              <b style={nuteeAWrapper}>NUTEE</b>
-            </a>
-          </Link>
-        </div>
-        {me ? 
-          <>
-            {showSearch ?
-              <>
-                <Input
-                  placeholder="검색어를 입력하세요."
-                  allowClear
-                  style={searchWrapper}
-                /><SearchOutlined style={showSearchWrapper} onClick={onSearch} /><CloseCircleOutlined onClick={onCloseSearch} style={closeIconWrapper} />
-              </> : <SearchOutlined style={showSearchWrapper} onClick={onShowSearch} />}
-          </>
-        : <></>}
-        {me ? (
+      <div style={logoWrapper}>
+        <Link href="/">
+          <a>
+            <img style={logoImgWrapper} src={'/nutee_circle.png'} alt="nutee" />
+          </a>
+        </Link>
+      </div>
+      <div style={nuteeWrapper}>
+        <Link href="/">
+          <a>
+            <b style={nuteeAWrapper}>NUTEE</b>
+          </a>
+        </Link>
+      </div>
+      {me ?
+        <>
+          {showSearch ?
+            <>
+              <Input
+                placeholder="검색어를 입력하세요."
+                allowClear
+                value={searchInput}
+                style={searchWrapper}
+                onChange={onChangeSearchInput}
+              /><SearchOutlined style={showSearchWrapper} onClick={onSearch} />
+            </> :
+            <>
+              <Popover placement="bottom" content={content} trigger="click">
+                <SearchOutlined style={showSearchWrapper} /></Popover>
+            </>
+          }
           <div style={logoutWrapper}>
             <Button
               style={logoutButtonWrapper}
               shape={"round"}
               onClick={onLogout}
-            >
-              <b>로그아웃</b>
+            ><b>로그아웃</b>
             </Button>
           </div>
-        ) : (
-            <></>
-          )}
+        </>
+        : <></>}
     </Row>
   );
 };
