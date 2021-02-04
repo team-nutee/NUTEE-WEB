063794fc44413ea2a59c0d7918da43531418cb77
gwonkim <wo41896804@gmail.com>
2021-02-04 23:13:02 +0900

    _document 수정

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -1,40 +1,41 @@
 import React from 'react';
-import Helmet from 'react-helmet';
-import PropTypes from 'prop-types';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
-    static getInitialProps(context) {
-        const sheet = new ServerStyleSheet();
-        const page = context.renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
-        const styleTags = sheet.getStyleElement();
-        return { ...page, helmet: Helmet.renderStatic(), styleTags };
+export default class MyDocument extends Document {
+  static async getInitalProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      sheet.seal();
     }
+  }
 
-    render() {
-        const { htmlAttributes, bodyAttributes, ...helmet } = this.props.helmet;
-        const htmlAttrs = htmlAttributes.toComponent();
-        const bodyAttrs = bodyAttributes.toComponent();
-
-        return (
-            <Html {...htmlAttrs}>
-                <Head>
-                    {this.props.styleTags}
-                    {Object.values(helmet).map(el => el.toComponent())}
-                </Head>
-                <body {...bodyAttrs}>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
-
-MyDocument.propTypes = {
-    helmet: PropTypes.object.isRequired,
-    styleTags: PropTypes.object.isRequired,
-};
-
-export default MyDocument;
