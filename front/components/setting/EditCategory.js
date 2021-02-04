15a9beaf530ac7407c689652650b7913f53fee60
gwonkim <wo41896804@gmail.com>
2021-02-05 01:14:22 +0900

    Setting page(5)
    EditCategory, EditMajor 완성

---
+++ front/components/setting/EditCategory.js
@@ -0,0 +1,41 @@
+import React, { useCallback, useMemo, useState } from "react";
+import { Button } from "antd";
+import { EDIT_CATEGORY_REQUEST } from "../../reducers/user";
+import { useDispatch, } from "react-redux";
+import { interestsData } from '../../components/dummy'; //dummy
+import Select from 'react-select';
+
+const EditCategory = () => {
+  const [interests, setInterests] = useState([]);
+  const dispatch = useDispatch();
+
+  const onEditCategory = useCallback(() => {
+    dispatch({
+      type: EDIT_CATEGORY_REQUEST,
+      data: interests,
+    });
+  }, [interests]);
+
+  const pageWrapper = useMemo(() => ({ display: "flex", justifyContent: "center", marginTop: '70px', height: 'auto' }), []);
+  const buttonWrapper = useMemo(() => ({ background: "#13c276", color: "#fff", width: "80px", marginLeft: '10px', height: '40px' }), []);
+  const customStyles = useMemo(() => ({ control: (css) => ({ ...css, width: "300px", border: '3px solid #005000', }), }), []);
+
+  return (
+    <>
+      <div style={pageWrapper}>
+        <Select
+          isMulti
+          autoFocus
+          placeholder="선호하는 카테고리를 선택해주세요."
+          styles={customStyles}
+          name="user-interests"
+          onChange={setInterests}
+          options={interestsData}
+        />
+        <Button style={buttonWrapper} onClick={onEditCategory}>확인</Button>
+      </div>
+    </>
+  );
+};
+
+export default EditCategory; 
---
+++ front/components/setting/EditMajor.js
@@ -0,0 +1,41 @@
+import React, { useCallback, useMemo, useState } from "react";
+import { Button } from "antd";
+import { EDIT_MAJOR_REQUEST } from "../../reducers/user";
+import { useDispatch, } from "react-redux";
+import { interestsData } from '../../components/dummy'; //dummy
+import Select from 'react-select';
+
+const EditCategory = () => {
+  const [major, setMajor] = useState([]);
+  const dispatch = useDispatch();
+
+  const onEditMajor = useCallback(() => {
+    dispatch({
+      type: EDIT_MAJOR_REQUEST,
+      data: major,
+    });
+  }, [major]);
+
+  const pageWrapper = useMemo(() => ({ display: "flex", justifyContent: "center", marginTop: '70px', height: 'auto' }), []);
+  const buttonWrapper = useMemo(() => ({ background: "#13c276", color: "#fff", width: "80px", marginLeft: '10px', height: '40px' }), []);
+  const customStyles = useMemo(() => ({ control: (css) => ({ ...css, width: "300px", border: '3px solid #005000', }), }), []);
+
+  return (
+    <>
+      <div style={pageWrapper}>
+        <Select
+          isMulti
+          autoFocus
+          placeholder="자신의 전공을 선택해주세요."
+          styles={customStyles}
+          name="user-interests"
+          onChange={setMajor}
+          options={interestsData}
+        />
+        <Button style={buttonWrapper} onClick={onEditMajor}>확인</Button>
+      </div>
+    </>
+  );
+};
+
+export default EditCategory; 
