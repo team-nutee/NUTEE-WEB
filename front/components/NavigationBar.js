e6e004d6fab3daeede7e5a2024131e88e849b855
san9901 <san990190@gmail.com>
2021-02-16 21:55:11 +0900

    모바일 화면 nav 추가
    검색, 프로필, 로그아웃만 가능

--- front/components/NavigationBar.js
+++ front/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { Button, Input, Row, Popover } from "antd";
+import { Button, Input, Row } from "antd";
 import { SearchOutlined } from '@ant-design/icons';
 import Link from "next/link";
 import Router from "next/router";
@@ -9,7 +9,6 @@ import useInput from '../hooks/useInput';
 
 const NavigationBar = ({ me }) => {
   const [searchInput, onChangeSearchInput] = useInput('');
-  const [showSearch, setShowSearch] = useState(false);
   const dispatch = useDispatch();
 
   const onSearch = useCallback(() => {
@@ -24,22 +23,9 @@ const NavigationBar = ({ me }) => {
     });
   }, []);
 
-  useEffect(
-    function onSearchWidth() {
-      if ((window.innerWidth || document.body.clientWidth) > 700) {
-        setShowSearch(true);
-      } else {
-        setShowSearch(false);
-      }
-      window.addEventListener("resize", onSearchWidth);
-      return () => {
-        window.removeEventListener("resize", onSearchWidth);
-      };
-    });
-
   const wrapper = useMemo(() => ({ background: '#f0faf5', height: '65px', paddingTop: '15px', position: 'fixed', top: '0', zIndex: '5', width: '100%', }), []);
   /* 로고 */
-  const logoWrapper = useMemo(() => ({ float: "left", marginLeft: '7vw' }), []);
+  const logoWrapper = useMemo(() => ({ float: "left", marginLeft: '7vw' }), []); 
   const logoImgWrapper = useMemo(() => ({ height: '40px', width: '40px', margin: '-1px 3px 0 5px' }), []);
 
   /* NUTEE */
@@ -53,16 +39,6 @@ const NavigationBar = ({ me }) => {
   /* 로그아웃 */
   const logoutWrapper = useMemo(() => ({ float: 'right', marginRight: '7vw', }), []);
   const logoutButtonWrapper = useMemo(() => ({ background: '#13c276', marginRight: '10px', borderColor: '#fff', color: 'white' }), []);
-
-  const content = (
-    <>
-      <Input
-        placeholder="검색어를 입력하세요."
-        allowClear
-        style={searchWrapper}
-      /><SearchOutlined style={showSearchWrapper} onClick={onSearch} />
-    </>
-  );
 
   return (
     <Row style={wrapper}>
@@ -82,21 +58,15 @@ const NavigationBar = ({ me }) => {
       </div>
       {me ?
         <>
-          {showSearch ?
-            <>
-              <Input
-                placeholder="검색어를 입력하세요."
-                allowClear
-                value={searchInput}
-                style={searchWrapper}
-                onChange={onChangeSearchInput}
-              /><SearchOutlined style={showSearchWrapper} onClick={onSearch} />
-            </> :
-            <>
-              <Popover placement="bottom" content={content} trigger="click">
-                <SearchOutlined style={showSearchWrapper} /></Popover>
-            </>
-          }
+          <>
+            <Input
+              placeholder="검색어를 입력하세요."
+              allowClear
+              value={searchInput}
+              style={searchWrapper}
+              onChange={onChangeSearchInput}
+            /><SearchOutlined style={showSearchWrapper} onClick={onSearch} />
+          </>
           <div style={logoutWrapper}>
             <Button
               style={logoutButtonWrapper}
