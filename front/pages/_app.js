292a80c25a620b097fd20088e768b9fd1c32eb5e
gwonkim <wo41896804@gmail.com>
2021-03-09 00:12:51 +0900

    app.js 수정
    Helmet -> Head

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,47 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import Head from 'next/head';
 import wrapper from '../store/configureStore';
 
 const Nutee = ({ Component }) => (
   <>
-    <Helmet
-      title="NUTEE"
-      htmlAttributes={{ lang: 'ko' }}
-      meta={[{
-        charset: 'UTF-8',
-      }, {
-        name: 'viewport',
-        content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
-      }, {
-        'http-equiv': 'X-UA-Compatible', content: 'IE=edge',
-      }, {
-        name: 'description', content: '성공회대학교 통합 커뮤니티 서비스',
-      }, {
-        name: 'og:title', content: 'NUTEE',
-      }, {
-        name: 'og:description', content: '성공회대학교 통합 커뮤니티 서비스',
-      }, {
-        property: 'og:type', content: 'website',
-      }, {
-        property: 'og:image', content: 'localhost:80/favicon.ico',
-      }]}
-      link={[{
-        rel: 'icon', type: 'image/png', href: 'favicon.ico',
-      }, {
-        rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css',
-      }, {
-        rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css',
-      }, {
-        rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css',
-      }, {
-        rel: 'stylesheet', href: 'https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css',
-      }, {
-        rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css',
-      }, {
-        rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Do+Hyeon|Nanum+Gothic&display=swap',
-      }]}
-    />
+    <Head>
+      <meta charSet="utf-8" />
+      <title>NUTEE</title>
+      <htmlAttributes lang="ko" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover" />
+      <meta name="description" content="성공회대학교 통합 커뮤니티 서비스" />
+      <meta name="og:title" content="NUTEE" />
+      <meta name="og:description" content="성공회대학교 통합 커뮤니티 서비스" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="localhost:80/favicon.ico" />
+      <link rel="stylesheet" href="https://fonts.gstatic.com" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css" />
+      <link rel="stylesheet" href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Do+Hyeon|Nanum+Gothic&display=swap" />
+      <link rel="icon" type="image/png" href="favicon.ico" />
+    </Head>
     <Component />
   </>
 );
