000f01c8267d6f93ee94100786ee8f5f7d4b9f63
gwonkim <wo41896804@gmail.com>
2021-03-25 01:08:16 +0900

    saga accessToken 수정

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -3,28 +3,13 @@ import axios from 'axios';
 import user from './user';
 import post from './post';
 import notice from './notice';
-import { TARGET_URL } from '../static';
 
-axios.defaults.baseURL = TARGET_URL;
 axios.defaults.withCredentials = true;
+
 const accessToken = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
-if (accessToken === null) {
-  console.log('saga-index token null');
-} else {
+if (accessToken !== null) {
   axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
 }
-
-/*  axios.interceptors.request.use(
-    function (config) {
-        let accessToken = window.sessionStorage.getItem('accessToken');
-        config.headers.authorization = `Bearer ${accessToken}`;
-        return config;
-    },
-    function (error) {
-        console.log(error);
-        return Promise.reject(error);
-    }
-);  */
 
 export default function* rootSaga() {
   yield all([
