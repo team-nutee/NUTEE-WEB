9431b1c80eae01d28fbce925595af948773ba18f
WIN-T9S3SRNDAJU\Administrator <san9901ho@gmail.com>
2021-05-08 23:26:06 +0900

    Notice api 연결
    Notice api  연결 및 수정

--- front/reducers/notice.js
+++ front/reducers/notice.js
@@ -1,25 +1,130 @@
 import produce from '../util/produce';
 
 export const initialState = {
-  noticeContents: [], // 크롤링 데이터
+  noticeTitles: [], // 크롤링 데이터
   noticeHrefs: [], // 크롤링 데이터 링크
 };
 
-export const LOAD_NOTICE_REQUEST = 'LOAD_NOTICE_REQUEST';
-export const LOAD_NOTICE_SUCCESS = 'LOAD_NOTICE_SUCCESS';
-export const LOAD_NOTICE_FAILURE = 'LOAD_NOTICE_FAILURE';
+export const LOAD_HAKSA_NOTICE_REQUEST = 'LOAD_HAKSA_NOTICE_REQUEST';
+export const LOAD_HAKSA_NOTICE_SUCCESS = 'LOAD_HAKSA_NOTICE_SUCCESS';
+export const LOAD_HAKSA_NOTICE_FAILURE = 'LOAD_HAKSA_NOTICE_FAILURE';
+
+export const LOAD_SOOUP_NOTICE_REQUEST = 'LOAD_SOOUP_NOTICE_REQUEST';
+export const LOAD_SOOUP_NOTICE_SUCCESS = 'LOAD_SOOUP_NOTICE_SUCCESS';
+export const LOAD_SOOUP_NOTICE_FAILURE = 'LOAD_SOOUP_NOTICE_FAILURE';
+
+export const LOAD_HAKJUM_NOTICE_REQUEST = 'LOAD_HAKJUM_NOTICE_REQUEST';
+export const LOAD_HAKJUM_NOTICE_SUCCESS = 'LOAD_HAKJUM_NOTICE_SUCCESS';
+export const LOAD_HAKJUM_NOTICE_FAILURE = 'LOAD_HAKJUM_NOTICE_FAILURE';
+
+export const LOAD_JANGHAK_NOTICE_REQUEST = 'LOAD_JANGHAK_NOTICE_REQUEST';
+export const LOAD_JANGHAK_NOTICE_SUCCESS = 'LOAD_JANGHAK_NOTICE_SUCCESS';
+export const LOAD_JANGHAK_NOTICE_FAILURE = 'LOAD_JANGHAK_NOTICE_FAILURE';
+
+export const LOAD_ILBAN_NOTICE_REQUEST = 'LOAD_ILBAN_NOTICE_REQUEST';
+export const LOAD_ILBAN_NOTICE_SUCCESS = 'LOAD_ILBAN_NOTICE_SUCCESS';
+export const LOAD_ILBAN_NOTICE_FAILURE = 'LOAD_ILBAN_NOTICE_FAILURE';
+
+export const LOAD_HANGSA_NOTICE_REQUEST = 'LOAD_HANGSA_NOTICE_REQUEST';
+export const LOAD_HANGSA_NOTICE_SUCCESS = 'LOAD_HANGSA_NOTICE_SUCCESS';
+export const LOAD_HANGSA_NOTICE_FAILURE = 'LOAD_HANGSA_NOTICE_FAILURE';
 
 const reducer = (state = initialState, action) => produce(state, (draft) => {
   switch (action.type) {
-    case LOAD_NOTICE_REQUEST: {
+    case LOAD_HAKSA_NOTICE_REQUEST: {
       break;
     }
-    case LOAD_NOTICE_SUCCESS: {
-      draft.noticeContents = action.data.content;
-      draft.noticeHrefs = action.data.hrefs;
+    case LOAD_HAKSA_NOTICE_SUCCESS: {
+      draft.noticeTitles[0] = [];
+      draft.noticeHrefs[0] = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < 6; i++) {
+        draft.noticeTitles[0].push(action.data[i].title);
+        draft.noticeHrefs[0].push(action.data[i].href);
+      }
       break;
     }
-    case LOAD_NOTICE_FAILURE: {
+    case LOAD_HAKSA_NOTICE_FAILURE: {
+      break;
+    }
+    case LOAD_SOOUP_NOTICE_REQUEST: {
+      break;
+    }
+    case LOAD_SOOUP_NOTICE_SUCCESS: {
+      draft.noticeTitles[1] = [];
+      draft.noticeHrefs[1] = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < 6; i++) {
+        draft.noticeTitles[1].push(action.data[i].title);
+        draft.noticeHrefs[1].push(action.data[i].href);
+      }
+      break;
+    }
+    case LOAD_SOOUP_NOTICE_FAILURE: {
+      break;
+    }
+    case LOAD_HAKJUM_NOTICE_REQUEST: {
+      break;
+    }
+    case LOAD_HAKJUM_NOTICE_SUCCESS: {
+      draft.noticeTitles[2] = [];
+      draft.noticeHrefs[2] = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < 6; i++) {
+        draft.noticeTitles[2].push(action.data[i].title);
+        draft.noticeHrefs[2].push(action.data[i].href);
+      }
+      break;
+    }
+    case LOAD_HAKJUM_NOTICE_FAILURE: {
+      break;
+    }
+    case LOAD_JANGHAK_NOTICE_REQUEST: {
+      break;
+    }
+    case LOAD_JANGHAK_NOTICE_SUCCESS: {
+      draft.noticeTitles[3] = [];
+      draft.noticeHrefs[3] = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < 6; i++) {
+        draft.noticeTitles[3].push(action.data[i].title);
+        draft.noticeHrefs[3].push(action.data[i].href);
+      }
+      break;
+    }
+    case LOAD_JANGHAK_NOTICE_FAILURE: {
+      break;
+    }
+    case LOAD_ILBAN_NOTICE_REQUEST: {
+      break;
+    }
+    case LOAD_ILBAN_NOTICE_SUCCESS: {
+      draft.noticeTitles[4] = [];
+      draft.noticeHrefs[4] = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < 6; i++) {
+        draft.noticeTitles[4].push(action.data[i].title);
+        draft.noticeHrefs[4].push(action.data[i].href);
+      }
+      break;
+    }
+    case LOAD_ILBAN_NOTICE_FAILURE: {
+      break;
+    }
+    case LOAD_HANGSA_NOTICE_REQUEST: {
+      break;
+    }
+    case LOAD_HANGSA_NOTICE_SUCCESS: {
+      draft.noticeTitles[5] = [];
+      draft.noticeHrefs[5] = [];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < 6; i++) {
+        draft.noticeTitles[5].push(action.data[i].title);
+        draft.noticeHrefs[5].push(action.data[i].href);
+      }
+      break;
+    }
+    case LOAD_HANGSA_NOTICE_FAILURE: {
       break;
     }
     default: {
--- front/sagas/notice.js
+++ front/sagas/notice.js
@@ -1,37 +1,176 @@
 import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
-import { TARGET_URL } from '../static';
-import { LOAD_NOTICE_FAILURE,
-  LOAD_NOTICE_REQUEST,
-  LOAD_NOTICE_SUCCESS,
+import { LOAD_HAKSA_NOTICE_FAILURE,
+  LOAD_HAKSA_NOTICE_REQUEST,
+  LOAD_HAKSA_NOTICE_SUCCESS,
+  LOAD_SOOUP_NOTICE_REQUEST,
+  LOAD_SOOUP_NOTICE_SUCCESS,
+  LOAD_SOOUP_NOTICE_FAILURE,
+  LOAD_HAKJUM_NOTICE_REQUEST,
+  LOAD_HAKJUM_NOTICE_SUCCESS,
+  LOAD_HAKJUM_NOTICE_FAILURE,
+  LOAD_JANGHAK_NOTICE_REQUEST,
+  LOAD_JANGHAK_NOTICE_SUCCESS,
+  LOAD_JANGHAK_NOTICE_FAILURE,
+  LOAD_ILBAN_NOTICE_REQUEST,
+  LOAD_ILBAN_NOTICE_SUCCESS,
+  LOAD_ILBAN_NOTICE_FAILURE,
+  LOAD_HANGSA_NOTICE_REQUEST,
+  LOAD_HANGSA_NOTICE_SUCCESS,
+  LOAD_HANGSA_NOTICE_FAILURE,
 } from '../reducers/notice';
 
-function getNoticeAPI() {
-  return axios.get(`${TARGET_URL}/api/notice`);
+function getHaksaNoticeAPI() {
+  return axios.get('http://3.34.61.71:9709/crawl/haksa', { data: {} });
 }
 
-function* getNotice(action) {
+function* getHaksaNotice(action) {
   try {
-    const result = yield call(getNoticeAPI, action.data);
+    const result = yield call(getHaksaNoticeAPI, action.data);
     yield put({
-      type: LOAD_NOTICE_SUCCESS,
-      data: result.data,
+      type: LOAD_HAKSA_NOTICE_SUCCESS,
+      data: result.data.body,
     });
   } catch (e) {
     console.error(e);
     yield put({
-      type: LOAD_NOTICE_FAILURE,
+      type: LOAD_HAKSA_NOTICE_FAILURE,
+      error: e.name,
+    });
+  }
+}
+
+function getSooupNoticeAPI() {
+  return axios.get('http://3.34.61.71:9709/crawl/sooup', { data: {} });
+}
+
+function* getSooupNotice(action) {
+  try {
+    const result = yield call(getSooupNoticeAPI, action.data);
+    yield put({
+      type: LOAD_SOOUP_NOTICE_SUCCESS,
+      data: result.data.body,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOAD_SOOUP_NOTICE_FAILURE,
       error: e,
     });
   }
 }
 
-function* watchGetNotice() {
-  yield takeEvery(LOAD_NOTICE_REQUEST, getNotice);
+function getHakjumNoticeAPI() {
+  return axios.get('http://3.34.61.71:9709/crawl/hakjum', { data: {} });
+}
+
+function* getHakjumNotice(action) {
+  try {
+    const result = yield call(getHakjumNoticeAPI, action.data);
+    yield put({
+      type: LOAD_HAKJUM_NOTICE_SUCCESS,
+      data: result.data.body,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOAD_HAKJUM_NOTICE_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function getJanghakNoticeAPI() {
+  return axios.get('http://3.34.61.71:9709/crawl/janghak', { data: {} });
+}
+
+function* getJanghakNotice(action) {
+  try {
+    const result = yield call(getJanghakNoticeAPI, action.data);
+    yield put({
+      type: LOAD_JANGHAK_NOTICE_SUCCESS,
+      data: result.data.body,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOAD_JANGHAK_NOTICE_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function getIlbanNoticeAPI() {
+  return axios.get('http://3.34.61.71:9709/crawl/ilban', { data: {} });
+}
+
+function* getIlbanNotice(action) {
+  try {
+    const result = yield call(getIlbanNoticeAPI, action.data);
+    yield put({
+      type: LOAD_ILBAN_NOTICE_SUCCESS,
+      data: result.data.body,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOAD_ILBAN_NOTICE_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function getHangsaNoticeAPI() {
+  return axios.get('http://3.34.61.71:9709/crawl/hangsa', { data: {} });
+}
+
+function* getHangsaNotice(action) {
+  try {
+    const result = yield call(getHangsaNoticeAPI, action.data);
+    yield put({
+      type: LOAD_HANGSA_NOTICE_SUCCESS,
+      data: result.data.body,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOAD_HANGSA_NOTICE_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchGetHaksaNotice() {
+  yield takeEvery(LOAD_HAKSA_NOTICE_REQUEST, getHaksaNotice);
+}
+
+function* watchGetSooupNotice() {
+  yield takeEvery(LOAD_SOOUP_NOTICE_REQUEST, getSooupNotice);
+}
+
+function* watchGetHakjumNotice() {
+  yield takeEvery(LOAD_HAKJUM_NOTICE_REQUEST, getHakjumNotice);
+}
+
+function* watchGetJanghakNotice() {
+  yield takeEvery(LOAD_JANGHAK_NOTICE_REQUEST, getJanghakNotice);
+}
+
+function* watchGetIlbanNotice() {
+  yield takeEvery(LOAD_ILBAN_NOTICE_REQUEST, getIlbanNotice);
+}
+
+function* watchGetHangsaNotice() {
+  yield takeEvery(LOAD_HANGSA_NOTICE_REQUEST, getHangsaNotice);
 }
 
 export default function* noticeSaga() {
   yield all([
-    fork(watchGetNotice),
+    fork(watchGetHaksaNotice),
+    fork(watchGetSooupNotice),
+    fork(watchGetHakjumNotice),
+    fork(watchGetJanghakNotice),
+    fork(watchGetIlbanNotice),
+    fork(watchGetHangsaNotice),
   ]);
 }
